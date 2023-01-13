7893f65a0ddc750b198704fe8179d7d91d3df767
victorbzrra <victorbzrra@alu.ufc.br>
2023-01-13 18:44:54 -0300

    Start project.

---
+++ src/test/java/com/tds/shortener/ShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tds.shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
